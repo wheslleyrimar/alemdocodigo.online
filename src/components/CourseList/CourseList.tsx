9efe40d3e4d782d00b40920e0dd6a042b05cf22c
Wheslley Rimar <wheslley_rimar@hotmail.com>
2024-06-07 16:12:55 -0300

    fixing https

--- src/components/CourseList/CourseList.tsx
+++ src/components/CourseList/CourseList.tsx
@@ -16,7 +16,7 @@ const CourseList: React.FC = () => {
   useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const response = await fetch('http://18.227.134.20:80:8080/api/courses', {
+        const response = await fetch('https://18.227.134.20/api/courses', {
           headers: {
             Authorization: `Bearer ${auth.token}`,
           },
